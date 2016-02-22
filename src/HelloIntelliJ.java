32fc3c41fa4b2a995636fbd2a2f74d40628fc842
Sunny-Kwak <happysun2@gmail.com>
2016-02-22 17:02:11 +0900

    test intellij + git

---
+++ src/HelloIntelliJ.java
@@ -0,0 +1,8 @@
+/**
+ * Created by sun2 on 2016. 2. 22..
+ */
+public class HelloIntelliJ {
+    public static void main (String[] args) throws Exception {
+        System.out.print("Hello IntelliJ !!" );
+    }
+}
